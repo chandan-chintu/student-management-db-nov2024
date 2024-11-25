0ca956d181fe124c4e7b529738e802d0f94c6d0f
Chandan K M <chandan.km@tecnotree.com>
2024-11-25 22:42:23 +0530

    CRUD APIs with JPA and hibernate

--- src/main/java/com/jpahibernate/example/student_management_db/service/StudentService.java
+++ src/main/java/com/jpahibernate/example/student_management_db/service/StudentService.java
@@ -1,7 +1,61 @@
 package com.jpahibernate.example.student_management_db.service;
 
+import com.jpahibernate.example.student_management_db.model.Student;
+import com.jpahibernate.example.student_management_db.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service // it will contain the business logic of the application
 public class StudentService {
+
+    @Autowired // it will create Spring internal bean - object of spring IOC container
+    private StudentRepository studentRepository;
+
+    public String addStudent(Student student){
+        studentRepository.save(student);
+        return "Student saved into database successfully";
+    }
+
+    public List<Student> getAllStudents(){
+        List<Student> studentList = studentRepository.findAll();
+        return studentList;
+    }
+
+    public Student getStudentById(int studentId){
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        return studentOptional.get();// this will get student inside optional
+    }
+
+    public String deleteStudentById(int studentId){
+        studentRepository.deleteById(studentId);
+        return "Student with id : "+studentId+" got deleted";
+    }
+
+    public String updateStudentUsingPut(int studentId, Student newStudentRequest){
+        // find the student with id
+        // if student present update it
+        // else cannot update
+        Student student = getStudentById(studentId);
+        if(student!=null){
+            studentRepository.save(newStudentRequest);
+            return "Student updated";
+        }
+        return "Student not found cannot update";
+    }
+
+    public String updateStudentUsingPatch(int studentId, String grade){
+        // find the student with id
+        // if student present update it
+        // else cannot update
+        Student student = getStudentById(studentId);
+        if(student!=null){
+            student.setGrade(grade);
+            studentRepository.save(student);
+            return "Student updated";
+        }
+        return "Student not found cannot update";
+    }
 }
